234767af95e1cf1586f6fe15226153f5368ca989
roaming0 <136370983+roaming0@users.noreply.github.com>
2023-06-12 15:07:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ const userGenerator = require('./user-generator');
 const readlineSync = require('readline-sync')
 
 //Data generate
-users = [];
-quantity = readlineSync.question("How many users do you want to create? ");
+let users = [];
+const quantity = readlineSync.question("How many users do you want to create? ");
 for(let i = 0; i<quantity; i++){
     users.push(userGenerator(1))
 }
+
 //Data printer
 console.log(users);
